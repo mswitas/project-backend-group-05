270011c838b34cb6dc416994693c24bdc2b4c895
mswitas <mswitas@gmail.com>
2024-05-13 17:10:22 +0200

    add database connection and project structure

---
+++ server.js
@@ -0,0 +1,27 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8000;
+const URI_DATABASE = process.env.URI_DATABASE;
+
+const server = async () => {
+  try {
+    const mongooseOptions = {
+      //useUnifiedTopology: true,
+    };
+
+    await mongoose.connect(URI_DATABASE, mongooseOptions);
+    console.log('Database connection successful');
+
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Cannot connect to database');
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+server();

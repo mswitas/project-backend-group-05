89fd72225a1b2bfca3ad2f6248ec7bb06dee7e32
mswitas <mswitas@gmail.com>
2024-05-17 12:22:07 +0200

    fix database connection

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
-const URI_DATABASE =
-  process.env.URI_DATABASE || "mongodb://localhost:27017/Kapusta";
+const URI_DATABASE = process.env.URI_DATABASE;
 
 const server = async () => {
   try {
@@ -14,7 +13,7 @@ const server = async () => {
     await mongoose.connect(URI_DATABASE, mongooseOptions);
     console.log("Database connection successful");
 
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log(`Server running. Use our API on port: ${PORT}`);
     });
   } catch (error) {

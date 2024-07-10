5d15877738695def532697738b0b128f9d02dd67
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-07-10 12:19:05 +0200

    changed months name to english

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -1,14 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("../../models/User");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../../models/User');
 
-router.post("/transaction/income", async (req, res) => {
+router.post('/transaction/income', async (req, res) => {
   try {
-    console.log("transaction/income");
+    console.log('transaction/income');
     if (!req.session.userId) {
-      return res.status(401).json({ error: "Unauthorized" });
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
     const { description, amount, date } = req.body;
@@ -16,13 +16,13 @@ router.post("/transaction/income", async (req, res) => {
     const user = await User.findById(req.session.userId);
 
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' });
     }
     user.transactions.push({
       description,
       amount,
       date,
-      category: "Przychód",
+      category: 'Przychód',
     });
 
     user.balance += amount;
@@ -36,21 +36,21 @@ router.post("/transaction/income", async (req, res) => {
     });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 });
 
-router.get("/transaction/income", async (req, res) => {
+router.get('/transaction/income', async (req, res) => {
   try {
-    console.log("transaction/income GET");
+    console.log('transaction/income GET');
     if (!req.session.userId) {
-      return res.status(401).json({ error: "Unauthorized" });
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
     const user = await User.findById(req.session.userId);
 
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' });
     }
 
     const monthStats = await User.aggregate([
@@ -60,25 +60,25 @@ router.get("/transaction/income", async (req, res) => {
         },
       },
       {
-        $unwind: "$transactions",
+        $unwind: '$transactions',
       },
       {
         $group: {
-          _id: { $month: "$transactions.date" },
-          total: { $sum: "$transactions.amount" },
+          _id: { $month: '$transactions.date' },
+          total: { $sum: '$transactions.amount' },
         },
       },
       {
         $project: {
           _id: 0,
-          month: "$_id",
+          month: '$_id',
           total: 1,
         },
       },
     ]);
 
     const monthData = {};
-    monthStats.forEach((stat) => {
+    monthStats.forEach(stat => {
       const monthName = getMonthName(stat.month);
       monthData[monthName] = stat.total;
     });
@@ -91,33 +91,37 @@ router.get("/transaction/income", async (req, res) => {
     res.json(response);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 });
 
 function getMonthName(monthNumber) {
   const monthNames = [
-    "Styczeń",
-    "Luty",
-    "Marzec",
-    "Kwiecień",
-    "Maj",
-    "Czerwiec",
-    "Lipiec",
-    "Sierpień",
-    "Wrzesień",
-    "Październik",
-    "Listopad",
-    "Grudzień",
+    'January ',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
   ];
+
+  if (monthNumber < 0 || monthNumber > 11) {
+    return 'Invalid month number';
+  }
   return monthNames[monthNumber];
 }
 
-router.post("/transaction/expense", async (req, res) => {
+router.post('/transaction/expense', async (req, res) => {
   try {
-    console.log("transaction/expense");
+    console.log('transaction/expense');
     if (!req.session.userId) {
-      return res.status(401).json({ error: "Unauthorized" });
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
     const { description, amount, date, category } = req.body;
@@ -125,7 +129,7 @@ router.post("/transaction/expense", async (req, res) => {
     const user = await User.findById(req.session.userId);
 
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' });
     }
     user.transactions.push({
       description,
@@ -145,25 +149,25 @@ router.post("/transaction/expense", async (req, res) => {
     });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 });
 
-router.get("/transaction/expense", async (req, res) => {
+router.get('/transaction/expense', async (req, res) => {
   try {
-    console.log("GET /transaction/expense");
+    console.log('GET /transaction/expense');
     if (!req.session.userId) {
-      return res.status(401).json({ error: "Unauthorized" });
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
     const user = await User.findById(req.session.userId);
 
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' });
     }
 
     const expenses = user.transactions.filter(
-      (transaction) => transaction.amount < 0
+      transaction => transaction.amount < 0
     );
 
     const monthStats = generateMonthStats(user.transactions);
@@ -174,7 +178,7 @@ router.get("/transaction/expense", async (req, res) => {
     });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 });
 
@@ -182,27 +186,27 @@ function generateMonthStats(transactions) {
   const monthStats = {};
 
   const polishMonths = {
-    0: "Styczeń",
-    1: "Luty",
-    2: "Marzec",
-    3: "Kwiecień",
-    4: "Maj",
-    5: "Czerwiec",
-    6: "Lipiec",
-    7: "Sierpień",
-    8: "Wrzesień",
-    9: "Październik",
-    10: "Listopad",
-    11: "Grudzień",
+    0: 'Styczeń',
+    1: 'Luty',
+    2: 'Marzec',
+    3: 'Kwiecień',
+    4: 'Maj',
+    5: 'Czerwiec',
+    6: 'Lipiec',
+    7: 'Sierpień',
+    8: 'Wrzesień',
+    9: 'Październik',
+    10: 'Listopad',
+    11: 'Grudzień',
   };
 
   for (const month in polishMonths) {
-    monthStats[polishMonths[month]] = "N/A";
+    monthStats[polishMonths[month]] = 'N/A';
   }
 
-  transactions.forEach((transaction) => {
+  transactions.forEach(transaction => {
     const month = new Date(transaction.date).getMonth();
-    if (monthStats[polishMonths[month]] === "N/A") {
+    if (monthStats[polishMonths[month]] === 'N/A') {
       monthStats[polishMonths[month]] = Math.abs(transaction.amount);
     } else {
       monthStats[polishMonths[month]] += Math.abs(transaction.amount);
@@ -212,34 +216,34 @@ function generateMonthStats(transactions) {
   return monthStats;
 }
 
-router.delete("/transaction", async (req, res) => {
+router.delete('/transaction', async (req, res) => {
   try {
-    console.log("DELETE /transaction");
+    console.log('DELETE /transaction');
     if (!req.session.userId) {
-      return res.status(401).json({ error: "Unauthorized" });
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
     const { transactionId } = req.body;
 
     if (!transactionId) {
-      return res.status(400).json({ error: "Transaction ID is required" });
+      return res.status(400).json({ error: 'Transaction ID is required' });
     }
     const user = await User.findById(req.session.userId);
 
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' });
     }
 
     const transactionToRemove = user.transactions.find(
-      (transaction) => transaction._id.toString() === transactionId
+      transaction => transaction._id.toString() === transactionId
     );
 
     if (!transactionToRemove) {
-      return res.status(404).json({ error: "Transaction not found" });
+      return res.status(404).json({ error: 'Transaction not found' });
     }
 
     user.transactions = user.transactions.filter(
-      (transaction) => transaction._id.toString() !== transactionId
+      transaction => transaction._id.toString() !== transactionId
     );
 
     user.balance += transactionToRemove.amount;
@@ -251,25 +255,25 @@ router.delete("/transaction", async (req, res) => {
     });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 });
 
 // Endpoint: GET /transaction/income-categories
-router.get("/transaction/income-categories", async (req, res) => {
+router.get('/transaction/income-categories', async (req, res) => {
   try {
     if (!req.session.userId) {
-      return res.status(401).json({ error: "Unauthorized" });
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
     const user = await User.findById(req.session.userId);
 
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' });
     }
 
     const categories = user.transactions.map(
-      (transaction) => transaction.category
+      transaction => transaction.category
     );
 
     const uniqueCategories = [...new Set(categories)];
@@ -277,37 +281,37 @@ router.get("/transaction/income-categories", async (req, res) => {
     res.json(uniqueCategories);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 });
 
-router.get("/transaction/expense-categories", async (req, res) => {
+router.get('/transaction/expense-categories', async (req, res) => {
   try {
     const expenseCategories = [
-      "Produkty",
-      "Alkohol",
-      "Rozrywka",
-      "Zdrowie",
-      "Transport",
-      "Wszystko dla domu",
-      "Technika",
-      "Komunikacja i media",
-      "Sport i hobby",
-      "Edukacja",
-      "Inne",
+      'Produkty',
+      'Alkohol',
+      'Rozrywka',
+      'Zdrowie',
+      'Transport',
+      'Wszystko dla domu',
+      'Technika',
+      'Komunikacja i media',
+      'Sport i hobby',
+      'Edukacja',
+      'Inne',
     ];
     res.json(expenseCategories);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 });
 
-router.get("/transaction/period-data", async (req, res) => {
+router.get('/transaction/period-data', async (req, res) => {
   try {
     const { date } = req.query;
     if (!date) {
-      return res.status(400).json({ error: "Date parameter is required" });
+      return res.status(400).json({ error: 'Date parameter is required' });
     }
 
     const startDate = new Date(`${date}-01`);
@@ -317,26 +321,26 @@ router.get("/transaction/period-data", async (req, res) => {
     const user = await User.findById(req.session.userId);
 
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' });
     }
 
     const transactionsInPeriod = user.transactions.filter(
-      (transaction) =>
+      transaction =>
         new Date(transaction.date) >= startDate &&
         new Date(transaction.date) < endDate
     );
 
     const incomes = transactionsInPeriod.filter(
-      (transaction) => transaction.amount > 0
+      transaction => transaction.amount > 0
     );
     const expenses = transactionsInPeriod.filter(
-      (transaction) => transaction.amount < 0
+      transaction => transaction.amount < 0
     );
 
     const incomesData = {};
     const expensesData = {};
 
-    incomes.forEach((income) => {
+    incomes.forEach(income => {
       const category = income.category;
       if (!incomesData[category]) {
         incomesData[category] = { total: 0 };
@@ -348,7 +352,7 @@ router.get("/transaction/period-data", async (req, res) => {
       incomesData[category][income.description] += income.amount;
     });
 
-    expenses.forEach((expense) => {
+    expenses.forEach(expense => {
       const category = expense.category;
       if (!expensesData[category]) {
         expensesData[category] = { total: 0 };
@@ -377,7 +381,7 @@ router.get("/transaction/period-data", async (req, res) => {
     res.json(response);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 });
 
